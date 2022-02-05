c31099d0280876ecce282912bc8f344860de1fc5
user <kavkras83@mail.ru>
2022-02-05 14:33:52 +0300

    refactoring: added classes Animals

---
+++ homework_1/src/main/java/animal/Main.java
@@ -0,0 +1,24 @@
+package animal;/*
+ * Copyright (c) 2022 Tander, All Rights Reserved.
+ */
+
+/**
+ * Класс animal.Main
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		Animal[] animals = new Animal[]{
+			new Dog("Бобик"),
+			new Dog("Шурик"),
+			new DomesticCat("Машка"),
+			new Tiger("Шарфан")
+		};
+		for (Animal animal : animals) {
+			animal.run(600);
+			animal.swim(300);
+
+		}System.out.println(animal);
+	}
+}
---
+++ homework_1/src/main/java/animal/Tiger.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Tander, All Rights Reserved.
+ */
+
+package animal;
+
+/**
+ * Класс Tiger
+ */
+public class Tiger extends Animal {
+	public Tiger(String name) {
+		super(name);
+		coutAnimals++;
+	}
+
+	@Override
+	public void swim(long lengthSwim) {
+		System.out.println("Тигр не умеет плавать :(");
+
+	}
+
+	@Override
+	public void run(long lengthRun) {
+		if (lengthRun >= 0 && lengthRun <= 3000) {
+			System.out.println("Tигр " + this.name + " пробежал: " + lengthRun + " метров");
+		}
+		if (lengthRun > 3000) {
+			System.out.println("Тигр " + this.name + lengthRun + " метров не пробежит");
+		}
+		if (lengthRun<0) {
+			System.out.println("не допустимое значение!");
+		}
+
+	}
+}
