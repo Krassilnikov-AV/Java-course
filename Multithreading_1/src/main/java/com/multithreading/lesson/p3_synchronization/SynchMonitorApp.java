a5b6cea738f97f19c35a4f7829ed4fd06a1ebea8
user <kavkras83@mail.ru>
2022-02-20 10:56:50 +0300

    refactoring: p3.synchronized

---
+++ Multithreading_1/src/main/java/com/multithreading/lesson/p3_synchronization/SynchMonitorApp.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Tander, All Rights Reserved.
+ */
+
+package com.multithreading.lesson.p3_synchronization;
+
+/**
+ * Класс SynchMonitorApp
+ */
+public class SynchMonitorApp {
+	private Object monitor = new Object();
+
+	public static void main(String[] args) {
+		SynchMonitorApp e2 = new SynchMonitorApp();
+		new Thread(() -> e2.method()).start();
+		new Thread(() -> e2.method()).start();
+		new Thread(() -> e2.method()).start();
+	}
+
+	private void method() {
+		try {
+			System.out.println("NonSynch-Begin " + Thread.currentThread().getName());
+			for (int i = 0; i < 3; i++) {
+				System.out.println("...");
+				Thread.sleep(200);
+			}
+			System.out.println("NonSynch-End " + Thread.currentThread().getName());
+			synchronized (monitor) {
+				System.out.println("Synch-Begin " + Thread.currentThread().getName());
+				for (int i = 0; i < 3; i++) {
+					System.out.println("...");
+					Thread.sleep(400);
+				}
+				System.out.println("Synch-End " + Thread.currentThread().getName());
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
