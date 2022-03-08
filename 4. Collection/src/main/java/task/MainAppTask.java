7fbf3c3dd03dc47e5c83aaf7e4f2d2b074dfe80a
user <kavkras83@mail.ru>
2022-03-08 13:15:06 +0300

    task collections_ point 1

--- 4. Collection/src/main/java/task/MainAppTask.java
+++ 4. Collection/src/main/java/task/MainAppTask.java
@@ -4,11 +4,24 @@
 
 package task;
 
+import java.util.Arrays;
+
+/**
+ * Класс MainAppTask
+ */
 public class MainAppTask {
 
-//	private ;
-	public void swappingPlaces(String[] arr) {
+	public static void main(String[] args) {
+		String[] arr = {"A", "B", "C", "E", "D"};
+		System.out.println("Начальный массив: " + Arrays.toString(arr));
 
+		swap(arr, 3, 4);
+		System.out.println("Результат: " + Arrays.toString(arr));
+	}
 
+	public static void swap(Object[] arr, int n1, int n2) {
+		Object obj = arr[n1];
+		arr[n1] = arr[n2];
+		arr[n2] = obj;
 	}
 }
