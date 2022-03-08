2844b5c0a7ecdc8dfc82a8a484b74ccb7dbb54dd
user <kavkras83@mail.ru>
2022-03-08 11:26:54 +0300

    сollection

--- 4. Collection/src/main/java/collection/lesson/MainApp.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2022 Tander, All Rights Reserved.
- */
-
-package collection.lesson;
-
-public class MainApp {
-}
---
+++ 4. Collection/src/main/java/task/MainAppTask.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Tander, All Rights Reserved.
+ */
+
+package task;
+
+public class MainAppTask {
+
+//	private ;
+	public void swappingPlaces(String[] arr) {
+
+
+	}
+}
