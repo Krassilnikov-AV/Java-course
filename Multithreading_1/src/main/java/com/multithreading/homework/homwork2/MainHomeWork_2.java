96185cd0c4b4fed517ec5ba154991182b39a339a
user <kavkras83@mail.ru>
2022-03-26 18:45:41 +0300

    refactoring: MainHomeWork_2 - solution

--- Multithreading_1/src/main/java/com/multithreading/homework/homwork2/MainHomeWork_2.java
+++ Multithreading_1/src/main/java/com/multithreading/homework/homwork2/MainHomeWork_2.java
@@ -4,24 +4,33 @@
 
 package com.multithreading.homework.homwork2;
 
+import java.util.concurrent.*;
+
 /**
  * Класс MainHomeWork_2
  */
 public class MainHomeWork_2 {
 
-	private static final int CARS_COUNT = 4;
+	public static final int CARS_COUNT = 4;
 
 	public static void main(String[] args) {
 		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+		CyclicBarrier barier = new CyclicBarrier(CARS_COUNT+1);
 		Race race = new Race(new Road(60), new Tunnel( ), new Road(40));
 		Car[] cars = new Car[CARS_COUNT];
 		for (int i = 0; i < cars.length; i++) {
-			cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+			cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barier);
 		}
 		for (int i = 0; i < cars.length; i++) {
 			new Thread(cars[i]).start();
 		}
-		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+		try {
+			barier.await();
+			System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+			barier.await();
+			System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
