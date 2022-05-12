ac0a366a8407c945b147667f0dedc0c9c98b976e
user <kavkras83@mail.ru>
2022-05-12 22:31:18 +0300

    refactoring: STEP_7 использование формы ввода для аутентификации

--- SpringSecurityDemo/src/main/java/net/springsecurity/demo/springsecuritydemo/config/SecurityConfig.java
+++ SpringSecurityDemo/src/main/java/net/springsecurity/demo/springsecuritydemo/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic();
+//			.httpBasic();
+//.and()
+			.formLogin()
+			.loginPage("/auth/login").permitAll()
+			.defaultSuccessUrl("/auth/success")
+			.and()
+			.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+			.invalidateHttpSession(true)
+			.clearAuthentication(true)
+			.deleteCookies("JSESSIONID")
+			.logoutSuccessUrl("/auth/login");
 	}
 
 	// загрузка пользователя по
