62962de485460a52f8991e5632c445d16ea2c74f
user <kavkras83@mail.ru>
2022-06-21 20:00:41 +0300

    refactoring: new project, springBoot starter

--- 14. Spring boot/Spring-Boot/src/main/java/com/Java_course/spring/boot/current/SpringBoot/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.Java_course.spring.boot.current.SpringBoot.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.Java_course.spring.boot.current.SpringBoot")
-public class AppConfig {
-}
