44909783f4baf79f9abf78e74ed80ca9bc812e50
user <kavkras83@mail.ru>
2022-06-22 21:08:54 +0300

    refactoring: example PorductService first

--- 14. Spring boot/src/main/java/com/Java_course/spring/boot/current/SpringBoot/controllers/ProductsController.java
+++ 14. Spring boot/src/main/java/com/Java_course/spring/boot/current/SpringBoot/controllers/ProductsController.java
@@ -1,55 +1,34 @@
 package com.Java_course.spring.boot.current.SpringBoot.controllers;
 
+import com.Java_course.spring.boot.current.SpringBoot.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+/**
+ * -при попытке постучаться по ссылке .../products,
+ * запрос перехватит метод showProductsList(Model model),
+ * он в свою очередь постучится в ProductService и вытащит
+ * от туда список всех товаров и достанет от туда список
+ * всех товаров из репозитори, а репозиторий из БД.
+ * Список товаров на фронтенд и покажет нам страницу
+ */
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
 
-    @GetMapping("/add")
-    public String method1() {
-        return "1";
-    }
-//    http://localhost:8189/Spring-Boot/products/add
+	private ProductsService productsService;
 
-    @GetMapping("/remove/{id}")
-    public String method2() {
-        return "1";
-    }
-    @GetMapping
-    public String method3() {
-        return "1";
-    }
-    @GetMapping("/edit/{id}")
-    public String method4() {
-        return "1";
-    }
-//    private ProductsService productsService;
-//
-//    @Autowired
-//    public void setProductsService(ProductsService productsService) {
-//        this.productsService = productsService;
-//    }
-//
-//    @GetMapping
-//    public String showProductsList(Model model) {
-//        Product product = new Product();
-//        model.addAttribute("products", productsService.getAllProducts());
-//        model.addAttribute("product", product);
-//        return "products";
-//    }
-//
-//    @PostMapping("/add")
-//    public String addProduct(@ModelAttribute(value = "product")Product product) {
-//        productsService.add(product);
-//        return "redirect:/products";
-//    }
-//
-//    @GetMapping("/show/{id}")
-//    public String showOneProduct(Model model, @PathVariable(value = "id") Long id) {
-//        Product product = productsService.getById(id);
-//        model.addAttribute("product", product);
-//        return "product-page";
-//    }
+	@Autowired
+	public void setProductsService(ProductsService productsService) {
+		this.productsService = productsService;
+	}
+
+	@GetMapping
+	public String showProductsList(Model model) {
+		model.addAttribute("products", productsService.getAllProducts());
+		return "products";
+	}
 }
---
+++ 14. Spring boot/src/main/java/com/Java_course/spring/boot/current/SpringBoot/services/ProductsService.java
@@ -0,0 +1,29 @@
+package com.Java_course.spring.boot.current.SpringBoot.services;
+
+
+import com.Java_course.spring.boot.current.SpringBoot.entities.Product;
+import com.Java_course.spring.boot.current.SpringBoot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * - ProductsService - выполняет полностью всю нашу работу
+ */
+@Service
+public class ProductsService {
+	// подключение к БД через репозиторий........
+	private ProductRepository productRepository;
+
+	@Autowired
+	public void setProductRepository(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	// получение всех товаров, через репозиторий
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+}
