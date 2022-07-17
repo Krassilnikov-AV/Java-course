22cdef7a6cd0991d36975bdf0c8f1d22c961b50e
user <kavkras83@mail.ru>
2022-07-17 14:58:57 +0300

    refactoring: 15_ Spring Security

--- 15. Spring Security_Data/src/main/java/com/Java_course/spring/boot/current/SpringBoot/configuration/AppConfig.java
+++ 15. Spring Security_Data/src/main/java/com/Java_course/spring/boot/current/SpringBoot/configuration/AppConfig.java
@@ -1,7 +1,6 @@
 package com.Java_course.spring.boot.current.SpringBoot.configuration;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan("com.Java_course.spring.boot.current.SpringBoot")
