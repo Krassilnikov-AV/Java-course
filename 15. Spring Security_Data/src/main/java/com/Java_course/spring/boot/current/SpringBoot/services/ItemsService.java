40cc9448398065331b31709ff11581a9eed61e9b
user <kavkras83@mail.ru>
2022-07-21 20:43:05 +0300

    refactoring: 15_ Spring Data_items_ JpaSpecificationExecutor

--- 15. Spring Security_Data/src/main/java/com/Java_course/spring/boot/current/SpringBoot/services/ItemsService.java
+++ 15. Spring Security_Data/src/main/java/com/Java_course/spring/boot/current/SpringBoot/services/ItemsService.java
@@ -3,10 +3,10 @@ package com.Java_course.spring.boot.current.SpringBoot.services;
 import com.Java_course.spring.boot.current.SpringBoot.entities.Item;
 import com.Java_course.spring.boot.current.SpringBoot.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ItemsService {
@@ -17,14 +17,21 @@ public class ItemsService {
 		this.itemRepository = itemRepository;
 	}
 
-	public List<Item> getAllItems() {
-		// получение в консоли данных по названию, вытащенных из БД при помощи Spring DAta
-		System.out.println(itemRepository.findByTitle("Tree"));
-		System.out.println(itemRepository.findByCostBetween(30, 70));
-		System.out.println(itemRepository.findByTitleWith("House"));
-		/**смотреть данные, на 5-ой странице в количестве 20 шт*/
-		itemRepository.findAll(PageRequest.of(5, 20));
+//	public List<Item> getAllItems() {
+//		// получение в консоли данных по названию, вытащенных из БД при помощи Spring DAta
+//		System.out.println(itemRepository.findByTitle("Tree"));
+//		System.out.println(itemRepository.findByCostBetween(30, 70));
+//		System.out.println(itemRepository.findByTitleWith("House"));
+//		/**смотреть данные, на 5-ой странице в количестве 20 шт*/
+//		itemRepository.findAll(PageRequest.of(5, 20));
+//
+//		return itemRepository.findAll();
+//	}
 
-		return itemRepository.findAll();
+	/**
+	 * Page - содержит информацию о страницах Item-ов, о номере текущей станицы и списо вобъектов на текущей странице
+	 */
+	public Page<Item> getItemsWithPaginAndFiltering(Specification<Item> specifications, Pageable pageable) {
+		return itemRepository.findAll(specifications, pageable);
 	}
 }
